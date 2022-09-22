a1af03ff50fdb9d19f56b96f9b47c36d7472157c
Diego A Asencio C <diegoasencio96@gmail.com>
2022-09-22 11:22:51 -0500

    adjust footer

--- contracts/views.py
+++ contracts/views.py
@@ -105,7 +105,7 @@ class PDFView(LoginRequiredMixin, TemplateView):
             'page-size': 'Letter',
             'encoding': 'UTF-8',
             'header-spacing': '15',
-            'footer-spacing': '-15',
+            'footer-spacing': '1',
             'margin-top': '1.0in',
             'margin-bottom': '1.0in',
             'margin-right': '0in',
