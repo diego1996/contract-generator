c74f95887592c126d25592c504850ade7a4a6378
Diego A Asencio C <diegoasencio96@gmail.com>
2022-09-23 08:03:54 -0500

    add debug logs and configure workers

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn core.wsgi:application --bind 0.0.0.0:8000
+gunicorn core.wsgi:application --workers 3 --log-level debug --bind 0.0.0.0:8000
