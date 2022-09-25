3aaa2b9e18a4aa104ec5a81d0113d13ea6162a5d
Diego A Asencio C <diegoasencio96@gmail.com>
2022-09-25 14:50:48 -0500

    runserver with gunicorn

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:8000
-# gunicorn core.wsgi:application --workers 3 --log-level debug --bind 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
+gunicorn core.wsgi:application --workers 3 --log-level debug --bind 0.0.0.0:8000
