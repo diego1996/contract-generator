f7e193470a5f636e344ceaaa970abb59e4c7f686
Diego A Asencio C <diegoasencio96@gmail.com>
2022-09-27 11:12:26 -0500

    otros camibos

--- contracts/resources.py
+++ contracts/resources.py
@@ -8,7 +8,7 @@ class ContractResource(resources.ModelResource):
     class Meta:
         model = Contract
         fields = (
-            'id', 'contract_date', 'contract_city__name', 'employer__name', 'employee__name', 'type__name',
-            'paid_period__name', 'position__name', 'city_work__name', 'base_salary', 'auxiliary_salary_reason',
-            'auxiliary_salary', 'start_work_date', 'place_work', 'duration'
+            'id', 'contract_date', 'resignation_date', 'contract_city__name', 'employer__name', 'employee__name',
+            'type__name', 'paid_period__name', 'position__name', 'city_work__name', 'base_salary',
+            'auxiliary_salary_reason', 'auxiliary_salary', 'start_work_date', 'place_work', 'duration'
         )
