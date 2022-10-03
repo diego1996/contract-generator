6b6f1fb75e15cbf8fc5156ecae30b49e6ab34a6a
Diego A Asencio C <diegoasencio96@gmail.com>
2022-10-03 10:51:22 -0500

    se agregan cositas

--- core/settings.py
+++ core/settings.py
@@ -133,8 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/admin/'
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://contratos.observatoriodesaludvillavicencio.org',
-    'https://contratos.observatoriodesaludvillavicencio.org',
+    'https://contratos.ingeomaq.com',
     'https://misdocumentos.live',
     'https://www.misdocumentos.live'
 ]
